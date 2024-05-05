c7518a16d58a5958870f5dc1068de0351b9eb37a
tibster <ttzombor@gmail.com>
2024-05-05 19:20:37 +0300

    removed unnecessary route

--- routes/routes.php
+++ routes/routes.php
@@ -12,5 +12,4 @@ Router::get('/user/create', 'user', [\App\Controllers\UserController::class, 'po
 Router::get('/user/login', 'user',  [\App\Controllers\AuthController::class, 'login']);
 
 Router::get('/register', 'registration');
-Router::get('/login', 'login');
 Router::get('/logout', '', [\App\Controllers\AuthController::class, 'logout']);
