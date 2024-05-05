73a589a92ab6eca064bfe2d43ae1093c69802b2e
tibster <ttzombor@gmail.com>
2024-05-05 19:11:22 +0300

    modified Middleware to accept params and validate by them. Plus multiple Middleware possibility added.

--- routes/routes.php
+++ routes/routes.php
@@ -7,8 +7,8 @@ Router::get('/', 'view');
 Router::get('/post', 'post', [\App\Controllers\PostController::class, 'get'], [\App\Http\Middleware\Auth::class]);
 Router::post('/posts', 'post', [\App\Controllers\PostController::class, 'get'], [\App\Http\Middleware\Auth::class]);
 
-Router::get('/users', 'user', [\App\Controllers\UserController::class, 'get'], [\App\Http\Middleware\Auth::class]);
-Router::get('/user/create', 'user', [\App\Controllers\UserController::class, 'post']);
+Router::get('/user', 'user', [\App\Controllers\DashboardController::class, 'get'], [\App\Http\Middleware\Auth::class]);
+Router::get('/user/create', 'user', [\App\Controllers\UserController::class, 'post'], [\App\Http\Middleware\Auth::class, \App\Http\Middleware\UserGroup::class => 'admin']);
 Router::get('/user/login', 'user',  [\App\Controllers\AuthController::class, 'login']);
 
 Router::get('/register', 'registration');
